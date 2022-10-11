1b977f82fedd82a8d79ca93c4e7b7771ef95d1be
ALVARO PARRES <parres@iteso.mx>
2022-10-10 23:00:33 -0500

    new fun dobleDouble

--- math.c
+++ math.c
@@ -10,3 +10,12 @@ int dobleInt(int a) {
     return a*2;
 }
 
+double dobleDouble(double a) {
+    if(a > MAX_DOUBLE) {
+        return -1;
+    } 
+
+    return a*2;
+}   
+}
+
--- math.h
+++ math.h
@@ -1,6 +1,7 @@
 /* Encabezados de Math.c */
 
 #define MAX_INT (2^sizeof(int))/2
+#define MAX_DOUBLE (2^sizeof(double))/2
 
 int dobleInt(int a);
-
+double dobleDouble(double a);        
